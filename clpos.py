fc910a78ffd2cf27527e2f55eb3aac04ab84e990
lucalocolocoloco <edijarqo@gmail.com>
2024-10-12 12:04:13 -0400

    Create clpos.py

---
+++ clpos.py
@@ -0,0 +1,20 @@
+import pyautogui
+import time
+import sys  # Import modul sys untuk keluar dari program
+
+# Setel fail-safe menjadi False
+pyautogui.FAILSAFE = False
+
+# Koordinat untuk klik
+coordinates = (789, 320)  # Ganti dengan koordinat yang diinginkan
+
+# Menunggu 3 detik sebelum melakukan klik
+time.sleep(3)
+
+# Menggerakkan mouse ke koordinat yang ditentukan dan melakukan klik
+pyautogui.moveTo(coordinates[0], coordinates[1], duration=0.5)  # Menggerakkan mouse dengan durasi 0.5 detik
+pyautogui.click()  # Melakukan klik
+
+# Mengakhiri program
+print("Klik sudah dilakukan. Program selesai.")
+sys.exit()  # Menutup program
