5893988431f58d835810b6322bd2b9a802455657
lucalocolocoloco <edijarqo@gmail.com>
2024-10-13 02:36:31 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 # Ubah direktori ke /home/coder/project
 cd /home/coder/project
+
+# Membuat pengguna baru 'fbbot' tanpa password dan menambahkannya ke grup 'sudo'
+sudo adduser --disabled-password --gecos "" fbbot
+sudo usermod -aG sudo fbbot
+
+# Membuat virtual environment Python
 python3 -m venv v 
 source v/bin/activate 
+
+# Menginstal paket Python dan Node.js
 pip install pyautogui 
 npm install axios
 npm install moment
 npm install puppeteer
 npm install dotenv
-# Mulai code-server tanpa otentikasi pada port 6090
+
+# Mulai code-server tanpa otentikasi pada port 8080
 echo "Starting code-server..."
 code-server --bind-addr 0.0.0.0:8080 --auth none . &
 
@@ -18,4 +27,3 @@ sleep 4
 # Mencegah script keluar dan container berhenti
 echo "Code-server is running. Keeping the container alive..."
 tail -f /dev/null
-
